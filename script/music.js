699fe35795fe1bab0a144083339c09ab01712cb1
tenrom <tenrom35@gmail.com>
2026-04-05 19:57:02 +0200

    Add files via upload

--- script/music.js
+++ script/music.js
@@ -453,11 +453,3 @@ player.on('ready', () => {
 })
 
 
-window.__onGCastApiAvailable = function(isAvailable) {
-  if (isAvailable) {
-    cast.framework.CastContext.getInstance().setOptions({
-      receiverApplicationId: chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID,
-      autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
-    });
-  }
-};
